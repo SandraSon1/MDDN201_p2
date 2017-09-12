f9b60f8c2318d8034ed5698ad36afe96eaf502dc
Sandra Son <sonsand@i-01-05-lab.mddn.vuw.ac.nz>
2017-09-12 14:15:14 +1200

    first upload

---
+++ script.js
@@ -0,0 +1,33 @@
+/*$(document).ready(function(){
+    $(".scroll").hide(0).delay(1000).fadeIn(3000)
+});
+
+
+$(window).scroll(function(){
+    $(".scroll").css("opacity", 1 - $(window).scrollTop() / 600);
+  });
+
+$(window).bind('scroll', function() {
+     if ($(window).scrollTop() > 1600) {
+         $('.scroll2').fadeOut();
+     }
+     else {
+         $('.scroll2').show();
+     }
+});*/
+$(document).ready(function(){
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
+
+
+
+
+});
+
